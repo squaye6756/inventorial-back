212bdc96722672f53d2b0e7a6cf7c61777537d21
Samuel Quaye <quayes955@gmail.com>
2021-12-01 02:30:38 -0500

    heroku deployment 2

--- inventorial_api/wsgi.py
+++ inventorial_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventorial_back.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventorial_api.settings')
 
 application = get_wsgi_application()
