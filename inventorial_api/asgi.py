e3dabf8154e6642d16a0f64e0fc37761a399bbab
Samuel Quaye <quayes955@gmail.com>
2021-12-01 02:31:40 -0500

    heroku deployment 3

--- inventorial_api/asgi.py
+++ inventorial_api/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventorial_back.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventorial_api.settings')
 
 application = get_asgi_application()
