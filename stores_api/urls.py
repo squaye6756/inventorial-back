37cb91fafc6847df488e357f6a070e5fadb8297c
Samuel Quaye <quayes955@gmail.com>
2021-12-08 09:46:18 -0500

    fixed (I hope) stores urls

--- stores_api/urls.py
+++ stores_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/stores', views.StoreList.as_view(), name='contact_list'),
-    path('api/stores/<int:pk>', views.StoreDetail.as_view(), name='contact_detail')
+    path('api/stores', views.StoreList.as_view(), name='store_list'),
+    path('api/stores/<int:pk>', views.StoreDetail.as_view(), name='store_detail')
 ]
