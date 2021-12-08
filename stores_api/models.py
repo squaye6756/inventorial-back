ce5139a7a8464cac9cf5096abcafc7e627e771c8
Samuel Quaye <quayes955@gmail.com>
2021-12-08 10:06:14 -0500

    attempting to make store inventory array field work properly

--- stores_api/models.py
+++ stores_api/models.py
@@ -4,4 +4,4 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 class Store(models.Model):
     name = models.CharField(max_length = 64)
-    inventory = ArrayField(models.IntegerField(), default = list)
+    inventory = ArrayField(models.IntegerField(), default = list, blank=True)
